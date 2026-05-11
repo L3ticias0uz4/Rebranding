bc55b175c3c8db9fa2e062ba87b1d66e1455fe5c
L3ticias0uz4 <leticiasouza56000@gmail.com>
2026-05-11 08:25:10 -0300

    feat: Add About component with styles for rebranding

---
+++ Rebranding/src/components/About.jsx
@@ -0,0 +1,27 @@
+import '../styles/About.css';
+import teamPhoto from '../assets/images/team.jpg';
+
+function About() {
+  return (
+    <section className="about-section" id="sobre">
+      <div className="about-text">
+        <p className="about-breadcrumb">Inicio &gt; Sobre</p>
+        <h1>Sobre nós</h1>
+        <p>
+          A Suporte de Domingo nasceu com o propósito de oferecer soluções
+          tecnológicas práticas, eficientes e personalizadas para empresas que
+          querem crescer com segurança.
+        </p>
+        <p>
+          Nosso time é formado por especialistas apaixonados por tecnologia e
+          resultados.
+        </p>
+      </div>
+      <div className="about-image">
+        <img src={teamPhoto} alt="Equipe Suporte de Domingo" />
+      </div>
+    </section>
+  );
+}
+
+export default About;
